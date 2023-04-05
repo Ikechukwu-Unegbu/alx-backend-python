796250a39a948dc501644689ad2f947ced5559ff
ikechukwu-unegbu <iunegbu94@yahoo.com>
2023-04-05 22:32:01 +0100

    fixing four

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -11,7 +11,7 @@ wait_random = __import__('0-basic_async_syntax').wait_random
 
 async def wait_n(n: int, max_delay: int) -> List[float]:
     
-    '''Execute wait_random n times.
+    '''Execute wait_random as much as n times.
     '''
 
     delay_time = await asyncio.gather(
