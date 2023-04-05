295aa10c657565ad78bec93fbd9b09494a04ba14
ikechukwu-unegbu <iunegbu94@yahoo.com>
2023-04-05 22:53:32 +0100

    first task

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python3
-'''First Task file
-'''
 import asyncio
 import random
 
 async def async_generator():
-    '''Async generator random
-    '''
+    """
+    An asynchronous generator that yields a random number between 0 and 10
+    after each asynchronous delay of 1 second. The generator loops 10 times.
+    """
     for i in range(10):
         await asyncio.sleep(1)
         yield random.randint(0, 10)
