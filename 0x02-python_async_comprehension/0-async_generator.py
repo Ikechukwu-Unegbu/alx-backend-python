c074a13480976f33a06e02c78a692e1d0198fa6c
ikechukwu-unegbu <iunegbu94@yahoo.com>
2023-04-05 22:50:34 +0100

    first task

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -1,8 +1,12 @@
-
+#!/usr/bin/env python3
+'''First Task file
+'''
 import asyncio
 import random
 
 async def async_generator():
+    '''Async generator random
+    '''
     for i in range(10):
         await asyncio.sleep(1)
         yield random.randint(0, 10)
